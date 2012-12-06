25b999146049b824294ae05e69f3d998c7650083
Jolanda Verhoef <jolanda@r2src.com>
2012-12-06 14:31:55 +0100

    removed init method. youtube api finished

--- playit.js
+++ playit.js
@@ -3,6 +3,7 @@ var width = 800;
 var height = 400;
 var id = 'player';
 var origin = "http://localhost";
+var player;
 
 // Load YouTube Frame API
 (function(){ //Closure, to not leak to the scope
@@ -12,28 +13,28 @@ var origin = "http://localhost";
      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
  })();
 
-init();
+// build the iframe element
+var newNode = document.createElement('iframe');
+newNode.width = width;
+newNode.height = height;
+newNode.id = id;
+// following lines might be necessary for vimeo fullscreen mode..
+//newNode.setAttribute('webkitAllowFullScreen', '');
+//newNode.setAttribute('mozallowfullscreen', '');
+//newNode.setAttribute('allowFullScreen', '');
+var oldNode = document.getElementById(id);
+oldNode.parentNode.replaceChild(newNode,oldNode);
+
 load();
-var player;
+
 function onYouTubeIframeAPIReady() {
     player = new YT.Player(id, {events: {'onStateChange': onPlayerStateChange}});
 }
 
 function onPlayerStateChange(event) {
-    alert('hoi');
-}
-function init() {
-    // build the iframe element
-    var newNode = document.createElement('iframe');
-    newNode.width = width;
-    newNode.height = height;
-    newNode.id = id;
-    // following lines might be necessary for vimeo fullscreen mode..
-    //newNode.setAttribute('webkitAllowFullScreen', '');
-    //newNode.setAttribute('mozallowfullscreen', '');
-    //newNode.setAttribute('allowFullScreen', '');
-    var oldNode = document.getElementById(id);
-    oldNode.parentNode.replaceChild(newNode,oldNode);
+    if (event.data === 0) {
+        load_next();
+    }
 }
 
 function load() {
